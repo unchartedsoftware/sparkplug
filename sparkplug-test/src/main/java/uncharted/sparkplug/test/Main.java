5f77b41abbc7f15f17df6479087692c762081d09
Michael Laccetti <michael@laccetti.com>
2015-09-18 17:02:43 -0400

    Oh Spark Spring Hell Turns out that you need to basically club Spring into working properly under Spark; all of the auto-start magic just doesn't work.  At all.

--- sparkplug-test/src/main/java/uncharted/sparkplug/test/Main.java
+++ sparkplug-test/src/main/java/uncharted/sparkplug/test/Main.java
@@ -2,14 +2,18 @@ package uncharted.sparkplug.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import uncharted.sparkplug.spring.SparkplugConfiguration;
 
 /**
  * Test harness for Sparkplug
  */
 @SpringBootApplication
-@Import(SparkplugConfiguration.class)
+@Import({SparkplugConfiguration.class, RabbitAutoConfiguration.class})
+@PropertySources(value = {@PropertySource("classpath:application.properties")})
 public class Main {
   public static void main(final String... args) {
     SpringApplication.run(Main.class, args);

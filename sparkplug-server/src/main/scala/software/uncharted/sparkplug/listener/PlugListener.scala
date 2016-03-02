711c9e4c02b9cd3f890e1830c8c379264f9aadf2
Michael Laccetti <mlaccetti@uncharted.software>
2016-03-02 18:51:39 -0500

    Getting this puppy building.

--- sparkplug-server/src/main/scala/software/uncharted/sparkplug/listener/PlugListener.scala
+++ sparkplug-server/src/main/scala/software/uncharted/sparkplug/listener/PlugListener.scala
@@ -19,18 +19,21 @@ package software.uncharted.sparkplug.listener
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
+import io.scalac.amqp.Connection
 
 class PlugListener {
   // streaming invoices to Accounting Department
   val connection = Connection()
+
   // create org.reactivestreams.Publisher
   val queue = connection.consume(queue = "invoices")
+
   // create org.reactivestreams.Subscriber
-  val exchange = connection.publish(exchange = "accounting_department",
-    routingKey = "invoices")
+  val exchange = connection.publish(exchange = "accounting_department", routingKey = "invoices")
 
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
+
   // Run akka-streams with queue as Source and exchange as Sink
   Source.fromPublisher(queue).map(_.message).runWith(Sink.fromSubscriber(exchange))
 }
